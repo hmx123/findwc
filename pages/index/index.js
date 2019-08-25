b7c62ab81b270b24999cdce3b3b69eae1ac9c2e4
unknown <17839192461@163.com>
2019-08-25 14:27:57 +0800

    第二页样式简单布局，简单缓存

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,6 +7,25 @@ Page({
     girlselected : false,
     boyselected : false
   },
+  onLoad: function() {
+    //判断用户是否设置性别
+    wx.getStorage({
+      key: 'gender',
+      success: function (res) {
+        //如果设置直接跳转到floor
+        wx.redirectTo({
+          url: '../floor/floor',
+        })
+      },
+      fail: function () {
+        wx.setStorage({
+          key: 'gender',
+          data: 'boy',
+        }),
+        console.log("设置缓存成功");
+      }
+    })     
+  },
   SelectHandle: function (e) {
     var gender = e.currentTarget.dataset.index
     if (gender === 'boy') {
